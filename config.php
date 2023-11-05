037fd8a70b97e35837f1e8ab90d438c42fae444b
ritikwan <113706704+ritikwan@users.noreply.github.com>
2023-11-06 03:40:33 +0530

    project uploaded

---
+++ config.php
@@ -0,0 +1,19 @@
+﻿<?php
+
+// DB credentials.
+define('DB_HOST','database-1.cv4jtfbxchp7.us-east-1.rds.amazonaws.com');
+define('DB_USER','admin');
+define('DB_PASS','admin123');
+define('DB_NAME','database');
+// Establish database connection.
+
+define('CURRENCY','Rs');
+try
+{
+$db = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+}
+catch (PDOException $e)
+{
+exit("Error: " . $e->getMessage());
+}
+?>
